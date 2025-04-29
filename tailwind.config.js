56114bac1b823263e585fd2cde8a5cf2ccb96f11
nin1 <gujiko.modebadze@gmail.com>
2025-04-29 23:13:16 +0400

    first commit. installed tailwind and added some elements to test it.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          pink: "#FF498A",
+          dark: "#333333",
+        },
+      },
+    },
+    plugins: [],
+  };
